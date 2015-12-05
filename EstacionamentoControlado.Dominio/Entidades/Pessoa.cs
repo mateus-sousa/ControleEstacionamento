3812b4f0992ca93486f1f713471db2c58f905f8e
mateusestacionamento <mateus.sousa157@gmail.com>
2015-12-05 14:58:44 -0200

    Projeto Pra Valer

---
+++ EstacionamentoControlado.Dominio/Entidades/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoControlado.Dominio.Entidades
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = " Nome requerido.")]
+        [Display(Name = "Nome:")]
+        public string NomePessoa { get; set; }
+
+        [Required(ErrorMessage = " CPF requerido.")]
+        [Display(Name = "CPF:")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = " RG requerido.")]
+        [Display(Name = "RG:")]
+        public string Rg { get; set; }
+
+        [Required(ErrorMessage = " E-mail requerido.")]
+        [Display(Name = "E-mail:")]
+        public string Email { get; set; }
+
+        public virtual ICollection<Veiculo> Veiculo { get; set; } 
+   
+
+
+    }
+}
---
+++ EstacionamentoControlado.Dominio/Entidades/Veiculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoControlado.Dominio.Entidades
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = " Marca do Veiculo é requerida.")]
+        [Display(Name = "Marca do Veiculo:")]
+        public string MarcaVeiculo { get; set; }
+
+        [Required(ErrorMessage = " Modelo do Veiculo é requerido.")]
+        [Display(Name = "Modelo do Veiculo:")]
+        public string ModeloVeiculo { get; set; }
+
+        [Required(ErrorMessage = " Placa do Veiculo é requerida.")]
+        [Display(Name = "Placa:")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = " Ano de lançamento de veiculo é requerido.")]
+        [Display(Name = "Ano de Lançamento:")]
+        public DateTime AnoVeiculo { get; set; }
+        
+    }
+}
